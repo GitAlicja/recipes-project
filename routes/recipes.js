3a469cf3b70b7b0e12a4bfa975273943363cdf3e
tmat <tamao.matsukawa@gmail.com>
2020-11-21 16:39:31 +0100

    half-finished

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,6 +1,27 @@
 
-// /all-recipes
+const express = require('express');
+const recipe = require('../models/Recipe.model');
+const router = express.Router();
+
+// /all-recipes 
+router.get('/recipes', (req, res, next) => {
+  
+  recipe.find().then((recipeFromDB) => {
+    console.log(recipeFromDB)
+    res.render('welcome', { recipes: recipeFromDB })
+})
+    
+});
+
+
+
 // /:id/edit
 // /:id/details
 // /all-recipes/filteredBy... (?)
 // /create-new
+
+
+
+
+
+module.exports = router;
