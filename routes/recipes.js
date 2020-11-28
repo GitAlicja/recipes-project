3fa2d5fa22a37ce1a991b4ec66a5aedba29b2f05
Alicja Slupska <ala.slupska@gmail.com>
2020-11-28 15:36:25 +0100

    rating feature in progress

--- routes/recipes.js
+++ routes/recipes.js
@@ -6,22 +6,64 @@ const router = express.Router();
 
 // /recipes 
 router.get('/recipes', (req, res, next) => {
-  let MealType = ["breakfast", "lunch", "dinner", "soup", "snacks", "dessert", "cake"]
+  let MealType = ["breakfast", "lunch", "dinner", "soup", "snacks", "dessert", "cake"];
   Recipe.find().then((recipeFromDB) => {
-    console.log("Meal type ================================>", MealType)
+    // console.log("Meal type ================================>", MealType)
     res.render('welcome', { recipes: recipeFromDB, MealType: MealType })
-  })
+  });
 
 });
 
 
 // /:id/details
 router.get('/recipes/:id', (req, res, next) => {
+  // array of possible scores added
+  let possibleScores = [1, 2, 3, 4, 5];
   const { id } = req.params;
   Recipe.findById(id)
     .then(recipeDetails => {
-      res.render('details', recipeDetails);
+      // passing the array possibleScores
+      res.render('details', { recipeDetails, possibleScores });
     })
+});
+
+
+// /:id/save-rating
+
+router.post('/recipes/:id/save-rating', (req, res, next) => {
+
+  if (!req.session.userId) {
+    res.redirect('/');
+  } else {
+
+    Recipe.findById(req.params.id).then(recipe => {
+      // check first if the user have already rated the recipe
+      // 'ratings' is the property name from the Recipes.Model
+      // 'user' is the property of the 'ratings' object from the Recipes.Model
+      const existingRating = recipe.ratings.find(rating => rating.user == req.session.userId);
+      if (existingRating) {
+        existingRating.rating = req.body.newRating;
+      } else {
+        const newRating = { user: req.session.userId, rating: req.body.newRating };
+        recipe.ratings.push(newRating);
+      }
+
+      recipe.avgRating = recipe.ratings.reduce((sum, entry) => sum + entry.rating, 0) / recipe.ratings.length;
+
+      
+      Recipe.update({ _id: recipe._id }, { ratings: recipe.ratings, avgRating: recipe.avgRating })
+        .exec()
+        .then(() => {
+
+          res.redirect('/recipes/' + req.params.id);
+        })
+
+
+
+
+
+    });
+  }
 });
 
 // /create-new
@@ -31,9 +73,9 @@ router.get('/create', (req, res) => res.render('create'));
 //post route to save new recipe to DB ** Removed ingredients and createdBy fields!!**
 router.post('/create', (req, res) => {
   console.log(req.body)
-  const { name, instructions, URL, image, prepTime, totalTime, typeOfMeal, typeOfRecipe, portions, rating } = req.body;
+  const { name, instructions, URL, image, prepTime, totalTime, typeOfMeal, typeOfRecipe, portions } = req.body;
 
-  Recipe.create({ name, instructions, URL, image, prepTime, totalTime, typeOfMeal, typeOfRecipe, portions, ingredients: [req.body.ingredients], rating, userID: req.session.user._id })
+  Recipe.create({ name, instructions, URL, image, prepTime, totalTime, typeOfMeal, typeOfRecipe, portions, ingredients: [req.body.ingredients], userID: req.session.user._id, ratings: [], avgRating: 0 })
     .then(() => res.redirect('/recipes'))
 });
 
@@ -85,11 +127,11 @@ router.get('/search', (req, response) => {
 })
 // /recipes/filteredBy... (?)
 router.get('/filter', (req, res) => {
-  Recipe.find({typeOfMeal:{$in: req.query.typeOfMeal}}).then((recipesFromDB) => {
-    if(recipesFromDB.length === 0){
+  Recipe.find({ typeOfMeal: { $in: req.query.typeOfMeal } }).then((recipesFromDB) => {
+    if (recipesFromDB.length === 0) {
       res.send("there is nothing base on ur filter")
     }
-    console.log("pleaseee worrrrrkkkkk",recipesFromDB)
+    console.log("pleaseee worrrrrkkkkk", recipesFromDB)
     res.render('recipes-search-results', { recipesFromDB })
   }).catch(error => {
     console.log("something went wrong to get filters fromdb", error)
