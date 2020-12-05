2ac0bf54e367374a73017471314e1838e4db235f
tmat <tamao.matsukawa@gmail.com>
2020-12-05 10:26:21 +0100

    tidying and making changes

--- routes/user.js
+++ routes/user.js
@@ -73,6 +73,7 @@ router.get('/profile/edit', (req, res, next) => {
 // POST /user/profile/edit
 // image is a parameter, the name attribute of the input field with the type of file
 router.post('/profile/edit', fileUploader.single('image'), (req, res, next) => {
+  console.log("user:-------------------->",req.body)
 
   if (!req.session.userId) {
     res.redirect('/');
